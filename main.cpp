9abd8c040cb852296c6bfc2766f5c882e97a428a
Matheus Vieira <matheusvhs@gmail.com>
2024-04-26 01:09:20 -0300

    Implementação da função Dijkstra

--- main.cpp
+++ main.cpp
@@ -9,26 +9,18 @@ Vitor Dias Carlos - 445 - GES
 //O código cria cidades com nome, código, e se tem ou nao centro Pokemon, e mostra a cidade cadastrada.
 
 #include <iostream>
-
 #include <string>
 #include <list>
 #include <climits>
 
-#include <list>
-
 using namespace std;
 
-<<<<<<< HEAD
 struct estrada{ //estrutura estrada
-=======
-struct estrada { //estrutura estrada
->>>>>>> e991924766d2b52ea86377672e350ca6d0ce5b23
   int origem;
   int destino;
   int distancia;
 };
 
-<<<<<<< HEAD
 struct Cidade{ //estrutura cidade
   int codigo;
   string nome;
@@ -158,54 +150,6 @@ void mostra_cidades(int num_cidades, Cidade cidade[]){
 
     for(it = cidade[i].vizinhos.begin(); it != cidade[i].vizinhos.end(); it++){
       cout<<cidade[it->destino].nome<<endl;
-=======
-struct Cidade { //estrutura cidade
-  int codigo;
-  string nome;
-  bool centro_pokemon;
-  list < estrada > vizinhos;
-};
-
-void cria_cidade(int num_cidades, int num_estradas, Cidade cidade[]) { //função para criar cidade
-
-  for (int i = 0; i < num_cidades; i++) {
-    cidade[i].codigo = i;
-    cout << "nome da cidade: ";
-    cin >> cidade[i].nome;
-    cout << "centro pokemon? (1 para sim, 0 para nao): ";
-    cin >> cidade[i].centro_pokemon;
-  }
-
-  int origem, destino, distancia;
-  for (int i = 0; i < num_estradas; i++) {
-    cout << "entre com o codigo da cidade de origem, da de destino, e a distancia: ";
-    cin >> origem >> destino >> distancia;
-
-    cidade[origem].vizinhos.push_back({origem,destino,distancia
-    });
-    cidade[destino].vizinhos.push_back({destino,origem,distancia
-    });
-
-  }
-
-}
-
-void mostra_cidades(int num_cidades, Cidade cidade[]) {
-  for (int i = 0; i < num_cidades; i++) {
-    cout << "codigo: " << cidade[i].codigo << endl;
-    cout << "nome: " << cidade[i].nome << endl;
-    if (cidade[i].centro_pokemon == 1) {
-      cout << "tem centro pokemon" << endl;
-    } else {
-      cout << "nao tem centro pokemon" << endl;
-    }
-    list < estrada > ::iterator it;
-
-    cout << "cidades vizinhas: ";
-
-    for (it = cidade[i].vizinhos.begin(); it != cidade[i].vizinhos.end(); it++) {
-      cout << it -> destino << endl;
->>>>>>> e991924766d2b52ea86377672e350ca6d0ce5b23
     }
 
   }
@@ -214,7 +158,6 @@ void mostra_cidades(int num_cidades, Cidade cidade[]) {
 
 int main() { //função principal
 
-<<<<<<< HEAD
   int var; //variável para escolher a opção
   
   cout<<"quantas cidades deseja cadastrar? ";
@@ -224,18 +167,9 @@ int main() { //função principal
   cin >> num_estradas;
 
   Cidade cidades[num_cidades];//variável para a cidade
-=======
-  int
-  var; //variável para escolher a opção
->>>>>>> e991924766d2b52ea86377672e350ca6d0ce5b23
 
-  cout << "quantas cidades deseja cadastrar? ";
-  int num_cidades, num_estradas;
-  cin >> num_cidades;
-  cout << "quantas estradas deseja cadastrar? ";
-  cin >> num_estradas;
+  while(true){ //loop para escolher a opção
 
-<<<<<<< HEAD
     cout<<"1 - Cadastrar cidades"<<endl;
     cout<<"2 - Mostrar conteudo"<<endl;
     cout<<"3 - como chegar ao centro pokemon mais proximo"<<endl;
@@ -244,24 +178,11 @@ int main() { //função principal
     cin>>var; //escolhe a opção	
     
     int a, custos[num_cidades], b = -1;
-=======
-  Cidade cidades[num_cidades]; //variável para a cidade
 
-  while (true) { //loop para escolher a opção
->>>>>>> e991924766d2b52ea86377672e350ca6d0ce5b23
-
-    cout << "1 - Cadastrar cidades" << endl;
-    cout << "2 - Mostrar conteudo" << endl;
-    cout << "3 - Sair" << endl;
-
-    cin >>
-      var; //escolhe a opção	
-
-    switch (var) { //switch para escolher a opção
+  switch(var){ //switch para escolher a opção
 
     case 1: //cria a cidade
       cria_cidade(num_cidades, num_estradas, cidades);
-<<<<<<< HEAD
       break;
 
     case 2: //mostra o conteúdo do Grafo Cidades
@@ -298,23 +219,12 @@ int main() { //função principal
       break;
     
     case 4: //sai do programa
-=======
-      break;
-
-    case 2: //mostra o conteúdo da cidade
-
-      mostra_cidades(num_cidades, cidades);
-
-      break;
-
-    case 3: //sai do programa
->>>>>>> e991924766d2b52ea86377672e350ca6d0ce5b23
       return 0;
 
     default: //opção invalida
       break;
 
-    }
   }
+}
   return 0;
 }
